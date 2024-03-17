2b04339a820cf6bee2e78b59cd2cde6d6b3c5e39
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-03-17 22:31:53 +0530

    Update Step1.jsx

--- src/Step1.jsx
+++ src/Step1.jsx
@@ -42,21 +42,21 @@ function PersonalInfo({ onNext, data }) {
       <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
         <div style={{ display: 'flex', gap: '20px' }}>
           <div style={{ flex: 1, marginRight: '20px' }}>
-            <label>First Name</label>
+            <label>First Name</label><br />
             <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} style={{ width: '70%', padding: '4px' }} />
           </div>
           <div style={{ flex: 1 }}>
-            <label>Last Name</label>
+            <label>Last Name</label><br />
             <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} style={{ width: '70%', padding: '4px' }} />
           </div>
         </div>
         <div style={{ display: 'flex', gap: '20px' }}>
           <div style={{ flex: 1, marginRight: '20px' }}>
-            <label>Phone Number</label>
+            <label>Phone Number</label><br />
             <input type="number" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} style={{ width: '70%', padding: '4px' }} />
           </div>
           <div style={{ flex: 1 }}>
-            <label>Email Address</label>
+            <label>Email Address</label><br />
             <input type="email" name="emailAddress" value={formData.emailAddress} onChange={handleChange} style={{ width: '70%', padding: '4px' }} />
           </div>
         </div>
